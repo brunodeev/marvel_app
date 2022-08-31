2a84970e4a85b385d8064cf555788a07811e4767
Bruno César <bcgmeireles@gmail.com>
2022-08-31 16:09:25 -0300

    Projeto Finalizado

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -5,9 +7,66 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Página de Configurações'),
+    return Scaffold(
+      backgroundColor: Colors.black.withOpacity(0.88),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 17, 17, 17),
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Text(
+                              'Informações',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 35,
+                                fontFamily: 'Muli',
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 60),
+                            child: Text(
+                              'Este app é destinado para quem deseja ter um controle superficial de todos os filmes vistos da Marvel. O app conta com a ordem cronológica dos filmes, bem como a sinopse, nota e pôster dos mesmos.',
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                  fontFamily: 'Muli'),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 20),
+                            child: Text(
+                              'Copyright © 2022 | Marvel App',
+                              style: TextStyle(
+                                  color: Colors.white.withOpacity(0.2),
+                                  fontSize: 15),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
